5f540af14c6d71863cc0398c1d1f0864defb0d1c
Lemi Faber <>
2026-03-01 21:19:08 +0100

    Save data and generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 import { calculateCartQuantity, cart, removeFromCart, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
-import formatCurrency from '../utils/money.js';
+import { products, getProduct } from "../../data/products.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrency } from '../utils/money.js';
 
 export default function renderOrderSummary() {
 
@@ -13,22 +13,11 @@ export default function renderOrderSummary() {
 
   cart.forEach(cartItem => {
     const productId = cartItem.productId;
-    let matchingProduct;
-
-    products.forEach(product => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    })
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
-    let deliveryOption;
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    deliveryOptions.forEach(option => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    })
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'day');
     const dateString = deliveryDate.format('dddd, MMMM D');
