2fd52d9a29dfa3277e899ad7dfb2f05e38dcaae2
Lemi Faber <>
2026-03-06 20:05:58 +0100

    Create order using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export default function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -66,4 +67,24 @@ export default function renderPaymentSummary() {
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+  
+  document.querySelector('.js-place-order-button').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('unexpected error, pls try again later');
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
